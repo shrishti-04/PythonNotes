7ea862920ab057982f192cc6f8f925319d70e849
Shrishti <Shrishtisreyansh@gmail.com>
2021-09-29 18:39:59 +0530

    forth

--- operation.py
+++ operation.py
@@ -1,24 +1,37 @@
 a = 5
 b = 4
 print(a+b)
+# OUTPUT: 9
 
 c = 5
 d = 4
 print(c-d)
+# OUTPUT: 1
 
 e = 5
 f = 4
 print(e*f)
+# OUTPUT: 20
 
 g = 10
 h = 5
 print(g/h)
+# OUTPUT: 2.0
 
 print(2**10)
-# to get the and answer of 2 raise to 10
+# to get the answer of 2 raise to 10
+# OUTPUT: 1024
 
 print(10//2)
+# OUTPUT: 5
 
 i = 10
 j = 5
 print(i%j)
+# OUTPUT: 0
+
+# PARENTHESES
+# Use Python to calculate (((1+2)*3)/4)^5 
+
+print((((1+2)*3)/4)**5)
+# OUTPUT: 57.665
