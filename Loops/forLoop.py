23c8b7eb2888777109ff5d755d038c525b01b382
Shrishti Tiwari <shrishtisreyansh@gmail.com>
2023-01-05 14:34:18 +0530

    loops

--- Loops/forLoop.py
+++ Loops/forLoop.py
@@ -1,5 +1,5 @@
 # Example1: Sum_squares function, so that it returns the sum of all the squares of numbers between 0 and x (not included).
-# Remember that you can use the range(x) function to generate a sequence of numbers from 0 to x (not included).
+# Remember that you can use the range(x) function to generate a sequence of numbers from 0 to x (not included)
 
 def square(n):
     return n*n
